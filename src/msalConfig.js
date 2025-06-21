932349c62141d314fbff6f5869e2b08b60cea3ce
unknown <jonesm274@gmail.com>
2025-06-21 09:45:10 +0100

    updated login page

--- src/msalConfig.js
+++ src/msalConfig.js
@@ -2,7 +2,7 @@ export const msalConfig = {
   auth: {
     clientId: "6080bbaa-4454-430b-8295-8ffe42fb7595",
     authority: "https://login.microsoftonline.com/5762dbac-7fd6-408e-89ff-7ce903fa9a52",
-    redirectUri: "https://jolly-pebble-092f82b03.2.azurestaticapps.net",
+    redirectUri: "https://www.smartlessons.co.uk/",
   },
   cache: {
     cacheLocation: "localStorage",
