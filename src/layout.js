c7de81c02db5fc271ce8bf3a1d2cfd43a7a25364
unknown <jonesm274@gmail.com>
2025-06-22 21:20:43 +0100

    ui updates

--- src/layout.js
+++ src/layout.js
@@ -33,7 +33,7 @@ const Layout = ({ children }) => {
             src={dretaiLogo}
             alt="DRET.AI Logo"
             className="w-full h-full object-contain"
-            style={{ display: "block", marginTop: "1px" }}
+            style={{ display: "block", maxHeight: "48px" }}
           />
         </div>
         {isSignedIn && (
