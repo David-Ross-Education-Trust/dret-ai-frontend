5cfcc87d4b37c8582689fd3d7d819b004a3c7d92
unknown <mjones@dret.co.uk>
2025-06-23 22:39:45 +0100

    updates

---
+++ src/splash/SplashScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function SplashScreen() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "20vh" }}>
+      <h1>Welcome to Teaching Tools</h1>
+      <p>Choose your portal above!</p>
+    </div>
+  );
+}
