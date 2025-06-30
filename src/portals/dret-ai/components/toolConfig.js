27d1701cee7f1f8a0cfce21e40681cce206d0b0e
unknown <mjones@dret.co.uk>
2025-06-30 02:03:28 +0100

    updates

--- src/portals/dret-ai/components/toolConfig.js
+++ src/portals/dret-ai/components/toolConfig.js
@@ -55,14 +55,4 @@ export const toolsConfig = [
     component: CASEParagraphFeedbackStudent,
     comingSoon: false,
   },
-  { id: "006", comingSoon: true },
-  { id: "007", comingSoon: true },
-  { id: "008", comingSoon: true },
-  { id: "009", comingSoon: true },
-  { id: "010", comingSoon: true },
-  { id: "011", comingSoon: true },
-  { id: "012", comingSoon: true },
-  { id: "013", comingSoon: true },
-  { id: "014", comingSoon: true },
-  { id: "015", comingSoon: true }
 ];
