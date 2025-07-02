7d06138458ad8a3dc813113daf028f5ccd4d4cf4
unknown <mjones@dret.co.uk>
2025-07-02 21:03:19 +0100

    updates

--- src/portals/dret-analytics/reports/toolkit/AbbeyToolkit.js
+++ src/portals/dret-analytics/reports/toolkit/AbbeyToolkit.js
@@ -3,17 +3,18 @@ import { Search, X } from "lucide-react";
 import AnalyticsLayout from "../../components/layout";
 import excelIcon from "../../../../assets/excel-icon.png";
 
+// Update all file links to use ms-excel:ofe|u| prefix for opening in Excel app
 const dummyFiles = [
-  { name: "Attendance Tracker", url: "#", },
-  { name: "Staff Timetable", url: "#", },
-  { name: "Pupil Progress", url: "#", },
-  { name: "Yearly Budget", url: "#", },
-  { name: "Student Contacts", url: "#", },
-  { name: "Classroom Resources", url: "#", },
-  { name: "Assessment Results", url: "#", },
-  { name: "Curriculum Map", url: "#", },
-  { name: "Parent Communications", url: "#", },
-  { name: "Site Risk Assessment", url: "#", },
+  { name: "Attendance Tracker", url: "ms-excel:ofe|u|https://davidrosseducationtrust-my.sharepoint.com/:x:/r/personal/dsands_dret_co_uk/Documents/!%20D%20Sands/_Toolkit%20Work/Secondary/_Test%20Toolkit%20Secondary/DRET%20Academy%20Attendance%20Tracker.xlsx?d=wb1689788bf8845449857c94b11225d64&csf=1&web=1&e=reaBnc", },
+  { name: "Behaviour Tracker", url: "ms-excel:ofe|u|https://davidrosseducationtrust-my.sharepoint.com/:x:/r/personal/dsands_dret_co_uk/Documents/!%20D%20Sands/_Toolkit%20Work/Secondary/_Test%20Toolkit%20Secondary/DRET%20Academy%20Behaviour%20Tracker.xlsx?d=wc349673e6d7a44e09cbdcd89ae1723a6&csf=1&web=1&e=IdgMTI", },
+  { name: "Context", url: "ms-excel:ofe|u|https://davidrosseducationtrust-my.sharepoint.com/:x:/r/personal/dsands_dret_co_uk/Documents/!%20D%20Sands/_Toolkit%20Work/Secondary/_Test%20Toolkit%20Secondary/DRET%20Academy%20Context.xlsx?d=w33994516585149538bd74cda8b0792ba&csf=1&web=1&e=CkPlFf", },
+  { name: "Inclusion Map", url: "ms-excel:ofe|u|https://davidrosseducationtrust-my.sharepoint.com/:x:/r/personal/dsands_dret_co_uk/Documents/!%20D%20Sands/_Toolkit%20Work/Secondary/_Test%20Toolkit%20Secondary/DRET%20Academy%20Inclusion%20Map.xlsx?d=w33d80ebe64ea4e6e90494665fbb841e6&csf=1&web=1&e=2UCeKM", },
+  { name: "School Around The Child", url: "ms-excel:ofe|u|https://davidrosseducationtrust-my.sharepoint.com/:x:/r/personal/dsands_dret_co_uk/Documents/!%20D%20Sands/_Toolkit%20Work/Secondary/_Test%20Toolkit%20Secondary/DRET%20Academy%20School%20Around%20The%20Child.xlsx?d=w69d9ab610e184a148ac8d08776349951&csf=1&web=1&e=Z9HSnk", },
+  { name: "Year 10 Achievement Tracker", url: "ms-excel:ofe|u|https://davidrosseducationtrust-my.sharepoint.com/:x:/r/personal/dsands_dret_co_uk/Documents/!%20D%20Sands/_Toolkit%20Work/Secondary/_Test%20Toolkit%20Secondary/DRET%20Academy%20Year%2010%20Achievement%20Tracker.xlsx?d=w6cbafca169344da09ee01087be733234&csf=1&web=1&e=Y3FjDg", },
+  { name: "Year 11 Achievement Tracker", url: "ms-excel:ofe|u|https://davidrosseducationtrust-my.sharepoint.com/:x:/r/personal/dsands_dret_co_uk/Documents/!%20D%20Sands/_Toolkit%20Work/Secondary/_Test%20Toolkit%20Secondary/DRET%20Academy%20Year%2011%20Achievement%20Tracker.xlsx?d=wd959f0521b134fec8d2b8b7bc648fbd2&csf=1&web=1&e=Oo8Djj", },
+  { name: "Year 7 Achievement Tracker", url: "ms-excel:ofe|u|https://davidrosseducationtrust-my.sharepoint.com/:x:/r/personal/dsands_dret_co_uk/Documents/!%20D%20Sands/_Toolkit%20Work/Secondary/_Test%20Toolkit%20Secondary/DRET%20Academy%20Year%207%20Achievement%20Tracker.xlsx?d=w88214b61333e4c0883bb83d642ac61c2&csf=1&web=1&e=NxtdsN", },
+  { name: "Year 8 Achievement Tracker", url: "ms-excel:ofe|u|https://davidrosseducationtrust-my.sharepoint.com/:x:/r/personal/dsands_dret_co_uk/Documents/!%20D%20Sands/_Toolkit%20Work/Secondary/_Test%20Toolkit%20Secondary/DRET%20Academy%20Year%208%20Achievement%20Tracker.xlsx?d=w16c2c4465725409c9e0fd0a9999cd25b&csf=1&web=1&e=PWAmZn", },
+  { name: "Year 9 Achievement Tracker", url: "ms-excel:ofe|u|https://davidrosseducationtrust-my.sharepoint.com/:x:/r/personal/dsands_dret_co_uk/Documents/!%20D%20Sands/_Toolkit%20Work/Secondary/_Test%20Toolkit%20Secondary/DRET%20Academy%20Year%209%20Achievement%20Tracker.xlsx?d=w4f61e058619a4259b5d6fc21f74cc513&csf=1&web=1&e=fhg4TF", },
 ];
 
 const TRUST_GREEN = "#205c40";
