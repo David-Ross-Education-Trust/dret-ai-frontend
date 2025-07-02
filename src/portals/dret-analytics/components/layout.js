92d89ec14c847fcc9ec828050492077d9f8683e0
unknown <mjones@dret.co.uk>
2025-07-02 09:37:10 +0100

    updates

--- src/portals/dret-analytics/components/layout.js
+++ src/portals/dret-analytics/components/layout.js
@@ -6,7 +6,7 @@ import { FaUserCircle } from "react-icons/fa";
 import { FiChevronDown } from "react-icons/fi";
 import { HiChevronDoubleLeft, HiChevronDoubleRight } from "react-icons/hi";
 import { useLocation, Link } from "react-router-dom";
-import dretAnalyticsLogo from "../../assets/dretai-logo.png";
+import dretAnalyticsLogo from "../../../assets/dretai-logo.png";
 
 // Sidebar items
 const navItems = [
