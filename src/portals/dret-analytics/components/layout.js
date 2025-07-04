a2bf9ec6a054a0184102712fe0f0cd582cb256c0
unknown <mjones@dret.co.uk>
2025-07-04 15:59:32 +0100

    updates

--- src/portals/dret-analytics/components/layout.js
+++ src/portals/dret-analytics/components/layout.js
@@ -9,7 +9,7 @@ import dretAnalyticsLogo from "../../../assets/dret-analytics-logo.png";
 const navItems = [
   { label: "Favourites", to: "/analytics" },
   { label: "Education Dashboards", to: "/analytics/education" },
-  { label: "Education Toolkits", to: "/analytics/toolkit" },
+  { label: "Education Toolkits", to: "/analytics/toolkits" },
   { label: "Operations", to: "/analytics/operations" },
   { label: "Finance", to: "/analytics/finance" },
   { label: "HR", to: "/analytics/hr" },
