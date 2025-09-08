5253bf021dfa63a0c58e34b0c6af16436ac69fd8
unknown <mjones@dret.co.uk>
2025-09-08 22:31:32 +0100

    updates

--- src/portals/dret-analytics/reports/toolkit/primary/AbbeyConfig.js
+++ src/portals/dret-analytics/reports/toolkit/primary/AbbeyConfig.js
@@ -8,7 +8,7 @@ export const AbbeyConfig = [
     openInBrowserHref:
       "https://davidrosseducationtrust.sharepoint.com/:x:/r/sites/DRET-Data-ACA/Shared%20Documents/ACA/2025-26/ACA%2025-26%20Baseline%20Summary.xlsx?d=wdbead4b0f65f4ea69dfb62d0d5ed513a&csf=1&web=1&e=PDROJv",
     logoUrl: excelIcon,
-    sourceToolkit: "Abbey Toolkit",
+    sourceToolkit: "Abbey CE Academy",
   },
   {
     id: "Academy Context",
@@ -17,7 +17,7 @@ export const AbbeyConfig = [
     openInBrowserHref:
       "https://davidrosseducationtrust.sharepoint.com/:x:/r/sites/DRET-Data-ACA/Shared%20Documents/ACA/2025-26/ACA%20Academy%20Context%20(25-26).xlsx?d=w2c688eaf03cf426a8d83fa14eab5f94f&csf=1&web=1&e=bzvEhD",
     logoUrl: excelIcon,
-    sourceToolkit: "Abbey Toolkit",
+    sourceToolkit: "Abbey CE Academy",
   },
   {
     id: "Attendance Tracker",
@@ -26,7 +26,7 @@ export const AbbeyConfig = [
     openInBrowserHref:
       "https://davidrosseducationtrust.sharepoint.com/:x:/r/sites/DRET-Data-ACA/Shared%20Documents/ACA/2025-26/ACA%20Attendance%20Tracker%20(25-26).xlsx?d=w1a6a0139a9c14db5955c0f94c7d9008a&csf=1&web=1&e=Rabg30",
     logoUrl: excelIcon,
-    sourceToolkit: "Abbey Toolkit",
+    sourceToolkit: "Abbey CE Academy",
   },
   {
     id: "Behaviour Tracker",
@@ -35,7 +35,7 @@ export const AbbeyConfig = [
     openInBrowserHref:
       "https://davidrosseducationtrust.sharepoint.com/:x:/r/sites/DRET-Data-ACA/Shared%20Documents/ACA/2025-26/ACA%20Behaviour%20Tracker%20(25-26).xlsx?d=wc810e276110d43788ff7c4373f84905f&csf=1&web=1&e=tJH6gu",
     logoUrl: excelIcon,
-    sourceToolkit: "Abbey Toolkit",
+    sourceToolkit: "Abbey CE Academy",
   },
   {
     id: "Class On a Page",
@@ -44,7 +44,7 @@ export const AbbeyConfig = [
     openInBrowserHref:
       "https://davidrosseducationtrust.sharepoint.com/:x:/r/sites/DRET-Data-ACA/Shared%20Documents/ACA/2025-26/ACA%20COAP%20(25-26).xlsx?d=w6972539a37ec4da5904592ea58a100c0&csf=1&web=1&e=M7xdJq",
     logoUrl: excelIcon,
-    sourceToolkit: "Abbey Toolkit",
+    sourceToolkit: "Abbey CE Academy",
   },
   {
     id: "Inclusion Map",
@@ -53,7 +53,7 @@ export const AbbeyConfig = [
     openInBrowserHref:
       "https://davidrosseducationtrust.sharepoint.com/:x:/r/sites/DRET-Data-ACA/Shared%20Documents/ACA/2025-26/ACA%20Inclusion%20Map%20(25-26).xlsx?d=wbd1f8cd138204842a9b2df16e2c261cb&csf=1&web=1&e=em7ovj",
     logoUrl: excelIcon,
-    sourceToolkit: "Abbey Toolkit",
+    sourceToolkit: "Abbey CE Academy",
   },
   {
     id: "Principal Report to ASC",
@@ -62,7 +62,7 @@ export const AbbeyConfig = [
     openInBrowserHref:
       "https://davidrosseducationtrust.sharepoint.com/:x:/r/sites/DRET-Data-ACA/Shared%20Documents/ACA/2025-26/ACA%20Principal%20report%20to%20ASC%20(25-26).xlsx?d=wef88cc55258245dd91b32d2577568841&csf=1&web=1&e=ReH5sw",
     logoUrl: excelIcon,
-    sourceToolkit: "Abbey Toolkit",
+    sourceToolkit: "Abbey CE Academy",
   },
   {
     id: "School Around The Child",
@@ -71,6 +71,6 @@ export const AbbeyConfig = [
     openInBrowserHref:
       "https://davidrosseducationtrust.sharepoint.com/:x:/r/sites/DRET-Data-ACA/Shared%20Documents/ACA/2025-26/ACA%20School%20Around%20The%20Child%20(25-26).xlsx?d=w926b62118120402ab2e16c62263692e8&csf=1&web=1&e=MECGMvy",
     logoUrl: excelIcon,
-    sourceToolkit: "Abbey Toolkit",
+    sourceToolkit: "Abbey CE Academy",
   },
 ];
