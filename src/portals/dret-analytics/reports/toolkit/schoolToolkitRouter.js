292763ea02be1bbe08262b24f24a2382e1e4e565
unknown <mjones@dret.co.uk>
2025-09-13 20:03:33 +0100

    updates

--- src/portals/dret-analytics/reports/toolkit/schoolToolkitRouter.js
+++ src/portals/dret-analytics/reports/toolkit/schoolToolkitRouter.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import SchoolToolkit from "../../../components/SchoolToolkit";
+import SchoolToolkit from "../../../components/schoolToolkit";
 import { schoolToolkitConfigs } from "./allToolkits";
 
 export default function SchoolToolkitRouter() {
